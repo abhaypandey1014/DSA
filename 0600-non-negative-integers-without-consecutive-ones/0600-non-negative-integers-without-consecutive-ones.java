042b87af1a9cb4d25f9c796583934c8e2ad5ea7a
abhaypandey1014 <pandeyabhay1010@gmail.com>
2026-06-30 01:55:12 +0530

    Refactor solve method and enhance comments
    Refactor the solve method to improve clarity and add comments for better understanding. Adjust the dp array initialization for clarity.

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -2,12 +2,13 @@ class Solution {
     public int solve(String s,int idx,int dp[][][][],int tight,int start,int prev){
         if(idx==s.length()) return 1;
         if(dp[idx][tight][start][prev]!=-1) return dp[idx][tight][start][prev];
-        int limit = (tight==1)?s.charAt(idx)-'0':1;
+        int limit = (tight==1)?s.charAt(idx)-'0':1; /// max limit can be 1
         int ans = 0;
         for(int i = 0;i<=limit;i++){
             int newTight = (tight==1 && i==limit)?1:0;
             if(start==0 && i==0) ans += solve(s,idx+1,dp,newTight,0,0);
             else{
+                //consecutive 1 hai to skip kro wrna aage forward kro
                 if(start==1 && prev==1 && i==1) continue;
                 ans += solve(s,idx+1,dp,newTight,i,1);
             }
@@ -15,7 +16,7 @@ class Solution {
         return dp[idx][tight][start][prev] = ans;
     }
     public int help(String s){
-        int dp[][][][] = new int[s.length()+1][2][2][2];
+        int dp[][][][] = new int[s.length()+1][2][2][2];//idx,tight,start,prev
         for(int i = 0;i<s.length()+1;i++){
             for(int j = 0;j<2;j++){
                 for(int k = 0;k<2;k++){
@@ -28,6 +29,7 @@ class Solution {
         return solve(s,0,dp,1,0,0);
     }
     public int findIntegers(int n) {
+        //for binary conversion because we are dealing with binary form
         String s = Integer.toBinaryString(n);
         return help(s);
     }
