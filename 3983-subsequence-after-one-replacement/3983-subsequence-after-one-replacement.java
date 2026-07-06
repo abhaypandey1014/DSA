91008bc9d2b2929caf56321974818f522aca7383
abhaypandey1014 <pandeyabhay1010@gmail.com>
2026-07-06 20:09:21 +0530

    Time: 16 ms (50%), Space: 48.3 MB (0%) - LeetHub

---
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canMakeSubsequence(String s, String t) {
+        int n = s.length();
+        int pref[] = new int[n+1];
+        int suff[] = new int[n+1];
+        int m = t.length();
+        int j = 0;
+        pref[0] = -1;
+        for(int i = 0;i<n;i++){
+            //Index find kro jaha pr match kr rha hai 
+            while(j<m && t.charAt(j)!=s.charAt(i)) j++; 
+            if(j==m){//koi ni match hua abhi tak
+                for(int k = i;k<n;k++){
+                    pref[k+1] = m;
+                }
+                break;
+            }
+            //match ho ja to store krate jao
+            pref[i+1] = j;
+            j++;
+        }
+        //matlab ki last character tak match ho gya hai t string k
+        if(pref[n]!=m) return true;
+
+        suff[n] = m;
+        j = m-1;
+        for(int i = n-1;i>=0;i--){
+            //Index find kro jaha pr match kr rha hai 
+            while(j>=0 && t.charAt(j)!=s.charAt(i)) j--; 
+            if(j<0){//koi ni match hua abhi tak
+                for(int k = i;k>=0;k--){
+                    suff[k] = -1;
+                }
+                break;
+            }
+            //match ho ja to store krate jao
+            suff[i] = j;
+            j--;
+        }
+
+        for(int i = 0;i<n;i++){
+            if(pref[i]+1<suff[i+1]) return true;
+        }
+        return false;
+    }
+}
