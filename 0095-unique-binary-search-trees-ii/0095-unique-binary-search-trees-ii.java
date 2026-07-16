73f824f8d5d1e3aa0596a5b78334280730af5c9d
abhaypandey1014 <pandeyabhay1010@gmail.com>
2026-07-16 15:39:23 +0530

    Time: 2 ms (18.82%), Space: 46.1 MB (92.29%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -1,20 +1,3 @@
-/**
-class Solution {
-    public int numTrees(int n) {
-        int dp[] = new int[n+1];
-        dp[0] = 1;//Zero node
-        dp[1] = 1;//One Node
-        for(int i = 2;i<n+1;i++){
-            for(int j = 0;j<i;j++){
-                int left = dp[j];//i
-                int right = dp[i-j-1];//n-i-1
-                dp[i] += right*left;//Catalan of n = catalan of 0 * catalan of n-1
-            }
-        }
-        return dp[n];
-    }
-}
- */
 class Solution {
     HashMap<Pair,List<TreeNode>> map;
     class Pair{
@@ -31,9 +14,7 @@ class Solution {
             list.add(null);
             return list;
         }
-        if(map.containsKey(new Pair(st,en))) {
-            return map.get(new Pair(st,en));
-        }
+        if(map.containsKey(new Pair(st,en))) return map.get(new Pair(st,en));
         for(int i = st;i<=en;i++){
             List<TreeNode> left = help(st,i-1);
             List<TreeNode> right = help(i+1,en);
