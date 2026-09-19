bb7c646ca006303fc82ac7d1d6cc04407f4f3845
abhaypandey1014 <pandeyabhay1010@gmail.com>
2026-09-20 00:02:58 +0530

    Time: 545 ms (16.58%), Space: 98.2 MB (5.07%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,50 +1,31 @@
 class Solution {
+    public boolean poss(int i,int j,int dp[][],String s){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j]==1;
+        if(s.charAt(i)==s.charAt(j) && poss(i+1,j-1,dp,s)){
+            dp[i][j] = 1;
+            return true;
+        }
+        dp[i][j] = -1;
+        return false;
+    }
     public String longestPalindrome(String s) {
-        if(s.length()==1) return s;
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
-            sb.append('#');
-            sb.append(c);
-        }
-        sb.append('#');
-        String t = sb.toString();
-        int n = t.length();
-        int[] p = new int[n];
         int max = 0;
-        int centre = 0;
-        int l = 0;
-        int r = -1;
-        int k;
-        p[0] = 0;
-        for(int i = 1;i<n;i++){
-            // Rightmost bound se bahar chla gya matlab firse radius find kro
-            if(i>r) k = 0;
+        int prev = 0;
 
-            else{
-                int j = l+(r-i);
-                //left bound se bahar ni gya hai to same p[i] rhega
-                if(j-p[j]>l){
-                    p[i] = p[j];
-                    continue;
-                } 
-
-                else k = r-i;
-            }
-            //radius find kr rhe palindrom ka
-            while(i-k-1>=0 && i+k+1<n && t.charAt(i-k-1)==t.charAt(i+k+1)){
-                k++;
-            }
-            p[i] = k;
-            if(p[i]>max){
-                max = p[i];
-                centre = i;
-            }
-            if(i+k>r){
-                l = i-k;
-                r = i+k;
+        int n =  s.length();
+        int dp[][] = new int[n+1][n+1];
+        for(int i = 0;i<=n;i++) Arrays.fill(dp[i],-1);
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                if(poss(i,j,dp,s)){
+                    if(j-i+1>max){
+                        max = j-i+1;
+                        prev =  i;
+                    }
+                }
             }
         }
-        int start = (centre-max)/2;
-        return s.substring(start,start+max);
+        return s.substring(prev,prev+max);
     }
 }
